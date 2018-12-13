abd2d5b12e0396acb41637076f9ec8bb2ed1aa64
Edgar Albalate <dev.gardo@gmail.com>
2018-12-13 19:15:27 +0100

    Fixed generator issue

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Person = require('./lib/person.js')
 
 // function generatePeopleJSONArray ( n = 16, options = {}, sortOptions = {}) {
 function generatePeopleJSONArray (ops= {}) {
-  ops = Object.assign({n: 16, generator:(p)=> {return p.toJSON()}, options: {}}, ops)
+  ops = {n: 16, generator:(p)=> (p.toJSON()), ...ops}
   let array = []
   for (let i = 0; i < ops.n; i++) {
     array.push( ops.generator(Person.random(), i))
@@ -18,5 +18,5 @@ function generatePeopleJSONArray (ops= {}) {
 module.exports.generatePeopleJSONArray = generatePeopleJSONArray
 
 if (require.main == module) {
-  console.log( generatePeopleJSONArray({generator:(p) => {return {name: p.firstname + ' ' + p.lastname}}}) )
+  console.log( generatePeopleJSONArray({generator:(p) => ({name: p.fullname})}) )
 }
